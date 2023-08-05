faafa244a3a9930b985110b51bdb3bb5abf3ead1
Resvith <116066494+Resvith@users.noreply.github.com>
2023-08-06 00:15:15 +0200

    Config json file

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 import openai
 import tkinter.messagebox
 import customtkinter
-import asyncio
-import aiohttp
+import threading
+import json
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -14,10 +14,28 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
+        # Create config file if no exists:
+        if not (os.path.exists("config.json")):
+            self.create_default_config()
+            print("Debug, config file created.")
+
+        # Load config file:
+        with open("config.json", "r") as json_file:
+            config_data = json.load(json_file)
+            self.chat_count = config_data["chat_count"]
+            self.model_pref = config_data["user_preferences"]["model"]
+            self.max_tokens_pref = config_data["user_preferences"]["max_tokens"]
+            self.temperature_pref = config_data["user_preferences"]["temperature"]
+            self.theme_mode_pref = config_data["user_preferences"]["theme"]
+            self.window_width_pref = config_data["user_preferences"]["window_width"]
+            self.window_height_pref = config_data["user_preferences"]["window_height"]
+            self.fullscreened_pref = config_data["user_preferences"]["full-screened"]
+
         # Configure window:
         self.title("OpenAI API Interface")
-        self.geometry(f"{1200}x{600}")
         self.minsize(1100, 580)
+        self.geometry(f"{self.window_width_pref}x{self.window_height_pref}")
+        self.state("normal")
 
         # Configure grid layout (4x4):
         self.grid_columnconfigure(1, weight=1)
@@ -50,7 +68,7 @@ class App(customtkinter.CTk):
         self.chat_space_frame.grid(row=0, column=1, rowspan=3, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
         self.selected_model_label = customtkinter.CTkLabel(self.chat_space_frame, text="Selected Model:", anchor="center")
         self.selected_model_label.grid(row=0, column=0, padx=20, pady=(10, 10), sticky='ne')
-        self.selected_model_options = customtkinter.CTkOptionMenu(self.chat_space_frame, values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k"], command=self.change_model_event)
+        self.selected_model_options = customtkinter.CTkOptionMenu(self.chat_space_frame, values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4"], command=self.change_model_event)
         self.selected_model_options.grid(row=0, column=1, padx=20, pady=(10, 10), sticky='nw')
         self.chat_space = customtkinter.CTkTextbox(self.chat_space_frame, wrap="word")
         self.chat_space.grid(row=1, rowspan=2, column=0, columnspan=3, padx=(20, 0), pady=(20, 0), sticky="nsew")
@@ -64,13 +82,13 @@ class App(customtkinter.CTk):
         # Max tokens and temperature:
         self.max_tokens_label = customtkinter.CTkLabel(self.options_frame, text="Max Tokens:", anchor="center")
         self.max_tokens_label.grid(row=0, column=0, padx=(10, 5), pady=10, sticky='ne')
-        self.max_tokens_entry = customtkinter.CTkEntry(self.options_frame, textvariable=tkinter.StringVar(value="1000"))
+        self.max_tokens_entry = customtkinter.CTkEntry(self.options_frame)
         self.max_tokens_entry.grid(row=0, column=1, padx=5, pady=10, sticky='nw')
         self.temperature_label = customtkinter.CTkLabel(self.options_frame, text="Temperature:", anchor="center")
         self.temperature_label.grid(row=1, column=0, padx=(10, 5), pady=10, sticky='ne')
         self.temperature_sidebar = customtkinter.CTkSlider(self.options_frame, from_=0, to=2)
         self.temperature_sidebar.grid(row=1, column=1, padx=5, pady=10, sticky='nw')
-        self.temperature_value_label = customtkinter.CTkLabel(self.options_frame, text="1.00", anchor="center")
+        self.temperature_value_label = customtkinter.CTkLabel(self.options_frame, text=str(self.temperature_pref), anchor="center")
         self.temperature_value_label.grid(row=1, column=2, padx=(10, 5), pady=10, sticky='nw')
         self.temperature_sidebar.bind("<B1-Motion>", self.temperature_sidebar_event)
 
@@ -84,36 +102,46 @@ class App(customtkinter.CTk):
 
         # Set default values and configure:
         self.new_chat_button.configure(text="New Chat")
-        self.theme_mode_options.set("Dark")
+        self.theme_mode_options.set(self.theme_mode_pref)
         self.chat_space.configure(state="disabled")
         self.input.after(10, self.input.focus_set)
+        self.temperature_sidebar.set(self.temperature_pref)
+        self.selected_model_options.set(self.model_pref)
+        self.max_tokens_entry.insert(0, self.max_tokens_pref)
+        if self.fullscreened_pref:
+            self.after(1, self.make_window_fullscreen)
+
+    def make_window_fullscreen(self):
+        self.state('zoomed')
 
     def enter_clicked(self, event):
         if not (event.state & 0x1):  # Check if Shift key is not pressed
-            self.on_send_button_click()
+            threading.Thread(target=self.check_correct_input).start()
 
     def on_send_button_click(self):
+        threading.Thread(target=self.check_correct_input).start()
+
+    def check_correct_input(self):
         # Check if input is empty:
         prompt = self.input.get("1.0", tkinter.END)
-        if prompt == "Send a message" or not prompt.strip():
+        print(prompt)
+        if prompt == "Send a message\n" or not prompt.strip():
             return
+        self.api_request(prompt)
 
-        # Debug, GET model list:
-        # modellist = openai.Model.list()
-        # for model in modellist.data:
-        #     print(model.id)
-
-        loop = asyncio.new_event_loop()
-        asyncio.get_event_loop()
-        loop.run_until_complete(self.api_request(prompt))
-        loop.close()
-
-    async def api_request(self, prompt):
+    def api_request(self, prompt):
         # Get values:
         model = self.selected_model_options.get()
         max_tokens = int(self.max_tokens_entry.get())
         temperature = float(self.temperature_value_label.cget("text"))
         role = self.role_textbox.get("1.0", tkinter.END)
+
+
+
+        # Clear input:
+        self.chat_space.configure(state="normal")
+        self.chat_space.insert(tk.END, prompt)
+        self.input.delete("1.0", tkinter.END)
 
         # Execute prompt:
         openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -121,22 +149,48 @@ class App(customtkinter.CTk):
             model=model,
             temperature=temperature,
             max_tokens=max_tokens,
+            stream=True,
             messages=
             [{"role": "user", "content": role},
              {"role": "user", "content": prompt}
              ])
 
-        # Clear input:
-        self.chat_space.configure(state="normal")
-        self.chat_space.insert(tk.END, prompt)
-        self.input.delete("1.0", tkinter.END)
+        # Debug, check models:
+        model_list = openai.Model.list()
+        for model in model_list.data:
+            print(model.id)
 
         # Get response into chat space:
-        temp_message = chat_completion.choices[0].message
-        response = temp_message.content
-        self.chat_space.insert(tk.END, "\n" + response + "\n\n")
+        complete_message = ""
+        for chunk in chat_completion:
+            if chunk and chunk['choices'][0]['delta'] != {}:
+                chunk_message = chunk['choices'][0]['delta']['content']
+                complete_message += chunk_message
+                self.chat_space.insert(tk.END, chunk_message)
+                self.chat_space.see(tk.END)
+
+        self.chat_space.insert(tk.END, "\n\n")
         self.chat_space.configure(state="disabled")
-        print(response)
+
+    def create_default_config(self):
+        default_config_data = {
+            "chat_count": 0,
+            "user_preferences": {
+                "model": "gpt-4",
+                "max_tokens": 1000,
+                "temperature": 1.0,
+                "theme": "dark",
+                "window_width": 1400,
+                "window_height": 800,
+                "full-screened": False
+            }
+        }
+
+        with open("config.json", "w") as json_file:
+            json.dump(default_config_data, json_file)
+
+    def write_message_to_file(self, ):
+        pass
 
     def on_input_focus_in(self, event):
         if self.input.get("1.0", tk.END) == "Send a message\n":
